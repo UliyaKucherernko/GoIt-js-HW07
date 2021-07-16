920aa7ddd8e8c5480e468d5008b0d5b52f972768
UliyaKucherernko <ulyakycherenko@gmail.com>
2021-07-16 13:59:48 +0300

    hw 7
    JS hw 7

---
+++ js/task-01.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const list = document.querySelectorAll('li.item');
+console.log(`В списке ${list.length} категории.`);
+
+const ul = document.querySelectorAll('#categories>li');
+ul.forEach(el => {
+
+  console.log(
+    `Категория: ${el.firstElementChild.textContent}, Количество элементов: ${el.lastElementChild.children.length}`,
+  );
+});
---
+++ js/task-02.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+
+const ulEl = document.querySelector('#ingredients');
+
+const list = ingredients.reduce((str, item) => str + `<li>${item}</li>`, '');
+
+ulEl.innerHTML = list;
