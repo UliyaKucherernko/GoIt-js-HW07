98d626b40d832ac23aa23f07be64906dfc856ded
UliyaKucherernko <80216711+UliyaKucherernko@users.noreply.github.com>
2021-07-20 12:43:57 +0300

    исправление

--- js/task-03.js
+++ js/task-03.js
@@ -16,10 +16,18 @@ const images = [
   }
 ];
 
-const ulEl = document.querySelector('#gallery');
-images.forEach(el => {
-  ulEl.insertAdjacentHTML(
-    'beforeEnd',
-    `<li><img src = "${el.url}" alt = "${el.alt}"  width = "150" height = "100" /></li>`,
-  );
+// const ulEl = document.querySelector('#gallery');
+// images.forEach(el => {
+//   ulEl.insertAdjacentHTML(
+//     'beforeEnd',
+//     `<li><img src = "${el.url}" alt = "${el.alt}"  width = "150" height = "100" /></li>`,
+//   );
+// });
+
+
+const galleryList = document.querySelector('#gallery');
+const addGalleryListEl = images.map(element => {
+  const addGalleryListElImg = `<li class="gallery__item"><img src="${element.url}" alt="${element.alt}" width=320 height=150></li>`;
+  return addGalleryListElImg;
 });
+galleryList.insertAdjacentHTML("beforeend", addGalleryListEl.join(' '));
