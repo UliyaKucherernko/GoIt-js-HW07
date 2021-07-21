6aeee8934610dda0f447bbfb6c368aaada873ce9
UliyaKucherernko <80216711+UliyaKucherernko@users.noreply.github.com>
2021-07-21 14:34:39 +0300

    исправление

--- js/task-02.js
+++ js/task-02.js
@@ -1,16 +1,8 @@
-const ingredients = [
-  'Картошка',
-  'Грибы',
-  'Чеснок',
-  'Помидоры',
-  'Зелень',
-  'Приправы',
-];
-const ingredientsList = document.querySelector('#ingredients')
-const element = ingredients.map(option => {
-  const ingredientsListEl = document.createElement('li')
-  ingredientsListEl.textContent = option
-  console.log(ingredientsListEl)
-  return ingredientsListEl
-})
-ingredientsList.append(...element)
+const ingredients = ['Картошка','Грибы','Чеснок','Помидоры','Зелень','Приправы',];
+let ulList = document.getElementById("ingredients");
+const foodIngredients = ingredients.forEach(ingredient => {
+  let items = document.createElement("li");
+  items.innerHTML = ingredient;
+  ulList.append(items);
+  // parent.append(...items)
+});
