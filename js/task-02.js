7d04625e51530ea1bc2edf7c7c527fd315731dff
UliyaKucherernko <80216711+UliyaKucherernko@users.noreply.github.com>
2021-07-21 14:36:49 +0300

    исправление

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,8 @@
 const ingredients = ['Картошка','Грибы','Чеснок','Помидоры','Зелень','Приправы',];
+
 let ulList = document.getElementById("ingredients");
 const foodIngredients = ingredients.forEach(ingredient => {
   let items = document.createElement("li");
   items.innerHTML = ingredient;
   ulList.append(items);
-  // parent.append(...items)
 });
